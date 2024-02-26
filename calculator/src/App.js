f2dc92e1fb4728700960b2a4755c56b9f2800d70
saran303 <saran13051999@gmail.com>
2024-02-27 00:02:19 +0530

    fixed case

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./App.module.css"
+import styles from"./App.module.css"
 
 function App() {
   const[input, setInput] = useState("");
@@ -26,25 +26,27 @@ function App() {
       <h1>React Calculator</h1>
       <input value={input}/>
       <p>{result}</p>
-      <button onClick={() =>handleClick("7")}>7</button>
-      <button onClick={() =>handleClick("8")}>8</button>
-      <button onClick={() =>handleClick("9")}>9</button>
-      <button onClick={() =>handleClick("+")}>+</button>
-      <br />
-      <button onClick={() =>handleClick("4")}>4</button>
-      <button onClick={() =>handleClick("5")}>5</button>
-      <button onClick={() =>handleClick("6")}>6</button>
-      <button onClick={() =>handleClick("-")}>-</button>
-      <br />
-      <button onClick={() =>handleClick("1")}>1</button>
-      <button onClick={() =>handleClick("2")}>2</button>
-      <button onClick={() =>handleClick("3")}>3</button>
-      <button onClick={() =>handleClick("*")}>*</button>
-      <br />
-      <button onClick={() =>handleReset()}>C</button>
-      <button onClick={() =>handleClick("0")}>0</button>
-      <button onClick={() =>handleCalculate()}>=</button>
-      <button onClick={() =>handleClick("/")}>/</button>
+      <div className={styles.buttons}>
+        <button onClick={() =>handleClick("7")}>7</button>
+        <button onClick={() =>handleClick("8")}>8</button>
+        <button onClick={() =>handleClick("9")}>9</button>
+        <button onClick={() =>handleClick("+")}>+</button>
+
+        <button onClick={() =>handleClick("4")}>4</button>
+        <button onClick={() =>handleClick("5")}>5</button>
+        <button onClick={() =>handleClick("6")}>6</button>
+        <button onClick={() =>handleClick("-")}>-</button>
+
+        <button onClick={() =>handleClick("1")}>1</button>
+        <button onClick={() =>handleClick("2")}>2</button>
+        <button onClick={() =>handleClick("3")}>3</button>
+        <button onClick={() =>handleClick("*")}>*</button>
+        
+        <button onClick={() =>handleReset()}>C</button>
+        <button onClick={() =>handleClick("0")}>0</button>
+        <button onClick={() =>handleCalculate()}>=</button>
+        <button onClick={() =>handleClick("/")}>/</button>
+      </div>
     </div>
   );
 }
